63196c3ef9adee0b1fbc72da15e25390127a05bc
Sergey Kuleshov <code4builder@gmail.com>
2022-10-23 15:10:42 +0200

    Added multilanguage select

---
+++ dist/js/lang.js
@@ -0,0 +1,233 @@
+const langArr = {
+    "header-about" : {
+        "en": "ABOUT",
+        "it": "CHI SONO"
+    },
+    "header-projects" : {
+        "en": "PROJECTS",
+        "it": "PROGETTI"
+    },
+    "header-contact" : {
+        "en": "CONTACT",
+        "it": "CONTATTI"
+    },
+    "showcase-i-am" : {
+        "en": "I am Sergey",
+        "it": "Sono Sergey"
+    },
+    "showcase-text" : {
+        "en": "I specialize in .NET, HTML, CSS, SQL and BIM Technologies",
+        "it": "Sono specializzato in .NET, HTML, CSS, SQL e tecnologie BIM"
+    },
+    "btn-view-my-work" : {
+        "en": "View My Work",
+        "it": "Guarda il mio lavoro"
+    },
+    "home-a-i-specialize" : {
+        "en": "I Specialize In",
+        "it": "Sono specializzato in "
+    },
+    "home-a-stack" : {
+        "en": ".NET, HTML, CSS, SQL as well as BIM Technologies",
+        "it": ".NET, HTML, CSS, SQL e anche in tecnologie BIM (Building Information Modeling)"
+    },
+    "home-a-item-1" : { 
+        "en": "C#, WPF, LINQ and Entity Framework",
+        "it": "C#, WPF, LINQ e Entity Framework"
+    },
+    "home-a-item-2-header" : {
+        "en": "HTML and CSS",
+        "it": "HTML e CSS"
+    },
+    "home-a-item-2" : {
+        "en": "HTML5 and CSS3",
+        "it": "HTML5 e CSS3"
+    },
+    "home-a-item-3" : {
+        "en": "MySQL, PostgreSQL and MS SQL",
+        "it": "MySQL, PostgreSQL e MS SQL"
+    },
+    "home-a-item-4" : {
+        "en": "BIM Modeling and Coordination",
+        "it": "Modellazione in BIM e Coordinamento"
+    },
+    "home-b-header" : {
+        "en": "My Goals",
+        "it": "Obbiettivi"
+    },
+    "home-b" : {
+        "en": "All of my actions in life are based on these 4 principles: ",
+        "it": "Tutte le mie azioni nella vita si basano su questi 4 principi:"
+    },
+    "home-b-item-1-header" : {
+        "en": "Continuous Self-Development",
+        "it": "Sviluppo personale"
+    },
+    "home-b-item-1a" : {
+        "en": "I improve my skills every day: books, video courses on Coursera and Udemy, articles, etc.",
+        "it": "Ogni giorno miglioro le mie capacità: libri, videocorsi su Coursera e Udemy, articoli, ecc."
+    },
+    "home-b-item-1b" : {
+        "en": "I speak also 2 foreign languages",
+        "it": "Parlo anche 2 lingue straniere"
+    },
+    "home-b-item-2-header" : {
+        "en": "Achieving A Result",
+        "it": "Raggiungere un risultato"
+    },
+    "home-b-item-2a" : {
+        "en": "Consistently meeting objectives and success criteria within agreed deadlines.",
+        "it": "Raggiungere costantemente obiettivi e criteri di successo entro le scadenze concordate."
+    },
+    "home-b-item-2b" : {
+        "en": "Planning, prioritising, and organising workloads",
+        "it": "Pianificazione, definizione delle priorità e organizzazione dei carichi di lavoro"
+    },
+    "home-b-item-3-header" : {
+        "en": "Work-Life Balance",
+        "it": "Equilibrio tra lavoro e vita privata"
+    },
+    "home-b-item-3a" : {
+        "en": "I love active lifestyle: new countries, mountains, etc. ",
+        "it": "Mi piace lo stile di vita attivo: nuovi paesi, montagne, ecc."
+    },
+    "home-b-item-3b" : {
+        "en": "Also I'm a marathon runner",
+        "it": "Inoltre sono un maratoneta"
+    },
+    "home-b-item-4-header" : {
+        "en": "Follow the dream",
+        "it": "Segui il sogno"
+    },
+    "home-b-item-4a" : {
+        "en": "You have to dream before your dreams can come true.",
+        "it": "Devi sognare prima che i tuoi sogni possano diventare realtà"
+    },
+    "home-b-item-4b" : {
+        "en": "Never give up!",
+        "it": "Non mollare mai"
+    },
+
+    "about-a-header-lead" : {
+        "en": "About Me",
+        "it": "Chi sono"
+    },
+    "about-a-paragraph-lead" : {
+        "en": "Let me tell you a little about myself and what I do",
+        "it": "Vi parlo un po' di me"
+    },
+    "about-a-header" : {
+        "en": ".Net Developer and BIM Coordinator",
+        "it": "Sviluppatore .Net e coordinatore BIM"
+    },
+    "about-a-paragraph-1" : {
+        "en": "I graduated from University in 2009 with 5-years Master's degree in Mechanical Engineering. After a few years in construction, I started to automate some processes and I continued to dive into programming.",
+        "it": "Mi sono laureato all'Università nel 2009 con una laurea magistrale quinquennale in Ingegneria Meccanica. Dopo alcuni anni nel settore edile ho iniziato ad automatizzare alcuni processi e ho continuato ad immergermi nella programmazione."
+    },
+    "about-a-paragraph-2" : {
+        "en": "I constantly try to follow the latest trends in software design and .NET in particular.",
+        "it": "Cerco costantemente di seguire le ultime tendenze nello sviluppo di software e .NET in particolare."
+    },
+    "about-a-paragraph-3" : {
+        "en": "I like both frontend and backend development.",
+        "it": "Mi piace lo sviluppo sia del frontend che del backend."
+    },
+    "about-a-paragraph-4" : {
+        "en": "I feel comfortable working with following frontend technologies: HTML5 and CSS3 with SASS.",
+        "it": "Mi sento a mio agio a lavorare con le seguenti tecnologie frontend: HTML5 e CSS3 con SASS."
+    },
+    "about-a-paragraph-5" : {
+        "en": "Backend technologies: C#, WPF, LINQ, Entity Framework and SQL databases.",
+        "it": "Tecnologie di backend: C#, WPF, LINQ, Entity Framework e database SQL."
+    },
+    "about-a-paragraph-6" : {
+        "en": "Based in Treviso, Italy",
+        "it": "Abito a Treviso in Italia "
+    },
+
+    "about-b-header" : {
+        "en": "Technical Skills",
+        "it": "Abilità tecniche "
+    },
+    "about-b-item-1" : {
+        "en": "C#, WPF with MVVM design pattern, LINQ and Entity Framework",
+        "it": "C#, WPF con MVVM design pattern, LINQ e Entity Framework"
+    },
+    "about-b-item-2" : {
+        "en": "Creating of web-sites using HTML5 and CSS3",
+        "it": "Creazione di siti WEB usando HTML5 e CSS3 "
+    },
+    "about-b-item-3" : {
+        "en": "MySQL, PostgreSQL and MSSQL",
+        "it": "MySQL, PostgreSQL e MSSQL"
+    },
+    "about-b-item-4" : {
+        "en": "Creating plugins for Autodesk Revit",
+        "it": "Creazione di plugin per Autodesk Revit"
+    },
+    "about-b-item-5" : {
+        "en": "Web requests with REST API",
+        "it": "Web requests con REST API"
+    },
+    "about-b-item-6" : {
+        "en": "Using of GIT",
+        "it": "Uso di GIT"
+    },
+    
+
+    "projects-a-ragraph-6" : {
+        "en": "Based in Treviso, Italy",
+        "it": "Abito a Treviso in Italia "
+    },
+
+    "projects-a-header-lead" : {
+        "en": "My Projects",
+        "it": "Miei progetti"
+    },
+    "projects-a-header" : {
+        "en": "Check out some of my projects",
+        "it": "Scopri alcuni dei miei progetti"
+    },
+    "projects-item-1" : {
+        "en": "C#, WPF with MVVM pattern, REST API",
+        "it": "C#, WPF con MVVM pattern, REST API"
+    },
+    "projects-item-2" : {
+        "en": "C# and WPF",
+        "it": "C# e WPF"
+    },
+    "projects-item-3" : {
+        "en": "C#, WPF and Revit API",
+        "it": "C#, WPF con Revit API"
+    },
+    "projects-item-4" : {
+        "en": "C#, WPF and Revit API",
+        "it": "C#, WPF con Revit API"
+    },
+    
+    "contact-a-header-lead" : {
+        "en": "Contact Me",
+        "it": "Contattami"
+    },
+    "contact-a-header" : {
+        "en": "Here is how you can reach me",
+        "it": "Ecco come puoi contattarmi"
+    },
+    "contact-a-btn-submit" : {
+        "en": "Submit",
+        "it": "Invia"
+    },
+    "contact-b-item-2-header" : {
+        "en": "Address",
+        "it": "Indirizzo"
+    },
+    "contact-c-header" : {
+        "en": "Let's Start Your Next Project",
+        "it": "Iniziamo il tuo prossimo progetto"
+    },
+
+    "footer-developed-by" : {
+        "en": "Developed by Sergey Kuleshov, 2022",
+        "it": "Sviluppato da Sergey Kuleshov, 2022"
+    },
+}
